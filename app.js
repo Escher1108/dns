049a88806df8e809ad7702e0cdc40a68bea37066
Escher1108 <3095697814@qq.com>
2022-03-08 22:36:32 +0800

    add flies

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+
+
+const url = 'https://dns.nextdns.io/ee47c8/Escher-iphone';
+const PORT = process.env.PORT || 5000;
+// const url = 'https://www.baidu.com'
+
+// 配置
+const options = {
+    target: url, // 目标服务器 host
+    changeOrigin: true,               // 默认false，是否需要改变原始主机头为目标URL
+    ws: true,                         // 是否代理websockets
+};
+
+// Info GET endpoint
+app.get('/about', (req, res, next) => {
+    // res.send('本页面由Escher1108提供！');
+    res.sendFile(__dirname + '/about.html');
+});
+
+app.use('/', createProxyMiddleware(options));
+app.listen(PORT);
